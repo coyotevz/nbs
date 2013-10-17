9ba9c90d9b97d96ad018b7afa599f0074fa49018
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-10-17 03:30:10 -0300

    Adding shadow when scrolling @pos

--- nbs/web/static/js/views/pos/document_view.js
+++ nbs/web/static/js/views/pos/document_view.js
@@ -46,6 +46,20 @@ define([
         'top': this.$('header').outerHeight(),
         'bottom': this.$('footer').outerHeight(),
         'visibility': 'visible', // Avoid flickr unpositioned element
+      }).scroll(function(evt) {
+        var $t = $(evt.target);
+
+        if ($t.scrollTop() > 0) {
+          $t.prev().toggleClass("shadowed", true);
+        } else {
+          $t.prev().toggleClass("shadowed", false);
+        }
+
+        if ($t.children('table').outerHeight() - $t.scrollTop() > $t.height()) {
+          $t.next().toggleClass("shadowed", true);
+        } else {
+          $t.next().toggleClass("shadowed", false);
+        }
       });
 
       // Set focus on last item-row
