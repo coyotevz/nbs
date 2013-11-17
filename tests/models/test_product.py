c0a8349ed1fcc627fe772bf0ebdf785120e57e77
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-11-17 05:08:38 -0300

    Added some more tests for product module

--- tests/models/test_product.py
+++ tests/models/test_product.py
@@ -5,10 +5,157 @@ from decimal import Decimal
 from sqlalchemy.exc import IntegrityError
 
 from tests import TestCase
-from nbs.models import Product, PriceComponent
+from nbs.models import ProductCategory, Product, PriceComponent
+
+
+class TestProductCategory(TestCase):
+
+    def test_default_values(self):
+        pc = ProductCategory(description=u'pc')
+        self.db.session.add(pc)
+        self.db.session.commit()
+        assert pc.suggested_markup == None
+        assert pc.suggested_commission == None
+
+
+    def test_raises_with_null_description(self):
+        pc = ProductCategory(suggested_markup=Decimal('30'),
+                             suggested_commission=Decimal('1'))
+        self.db.session.add(pc)
+        with raises(IntegrityError):
+            self.db.session.commit()
+
+    def test_parent_commission(self):
+        pc_parent = ProductCategory(description=u'pc_parent',
+                                    suggested_commission=Decimal('2'))
+        pc_child_1 = ProductCategory(description=u'pc_child_1',
+                                     suggested_commission=Decimal('5'))
+        pc_child_2 = ProductCategory(description=u'pc_child_2')
+
+        pc_child_1.parent = pc_parent
+        pc_child_2.parent = pc_parent
+
+        self.db.session.add(pc_parent)
+        self.db.session.commit()
+
+        assert pc_parent.get_commission() == Decimal('2')
+        assert pc_child_1.get_commission() == Decimal('5')
+        assert pc_child_2.get_commission() == Decimal('2')
+
+        pc_child_11 = ProductCategory(description=u'pc_child_11')
+        pc_child_21 = ProductCategory(description=u'pc_child_21')
+
+        pc_child_11.parent = pc_child_1
+        pc_child_21.parent = pc_child_2
+
+        self.db.session.commit()
+
+        assert pc_child_11.get_commission() == Decimal('5')
+        assert pc_child_21.get_commission() == Decimal('2')
+
+    def test_parent_markup(self):
+        pc_parent = ProductCategory(description=u'pc_parent',
+                                    suggested_markup=Decimal('30'))
+        pc_child_1 = ProductCategory(description=u'pc_child_1',
+                                     suggested_markup=Decimal('65'))
+        pc_child_2 = ProductCategory(description=u'pc_child_2')
+
+        pc_child_1.parent = pc_parent
+        pc_child_2.parent = pc_parent
+
+        self.db.session.add(pc_parent)
+        self.db.session.commit()
+
+        assert pc_parent.get_markup() == Decimal('30')
+        assert pc_child_1.get_markup() == Decimal('65')
+        assert pc_child_2.get_markup() == Decimal('30')
+
+        pc_child_11 = ProductCategory(description=u'pc_child_11')
+        pc_child_21 = ProductCategory(description=u'pc_child_21')
+
+        pc_child_11.parent = pc_child_1
+        pc_child_21.parent = pc_child_2
+
+        self.db.session.commit()
+
+        assert pc_child_11.get_markup() == Decimal('65')
+        assert pc_child_21.get_markup() == Decimal('30')
+
+    def test_get_path(self):
+        pc_parent = ProductCategory(description=u'pc_parent')
+        pc_child_1 = ProductCategory(description=u'pc_child_1',
+                                     parent=pc_parent)
+        pc_child_2 = ProductCategory(description=u'pc_child_2',
+                                     parent=pc_parent)
+
+        pc_child_11 = ProductCategory(description=u'pc_child_11',
+                                       parent=pc_child_1)
+        pc_child_12 = ProductCategory(description=u'pc_child_12',
+                                       parent=pc_child_1)
+        pc_child_21 = ProductCategory(description=u'pc_child_21',
+                                       parent=pc_child_2)
+        pc_child_22 = ProductCategory(description=u'pc_child_22',
+                                       parent=pc_child_2)
+
+        pc_child_211 = ProductCategory(description=u'pc_child_211',
+                                          parent=pc_child_21)
+
+        self.db.session.add(pc_parent)
+        self.db.session.commit()
+
+        assert list(pc_parent.get_path()) == [pc_parent]
+        assert list(pc_child_1.get_path()) == [pc_parent, pc_child_1]
+        assert list(pc_child_22.get_path()) == [pc_parent, pc_child_2,
+                                                pc_child_22]
+        assert list(pc_child_211.get_path()) == [pc_parent, pc_child_2,
+                                                 pc_child_21, pc_child_211]
+
+    def test_get_children_recursively(self):
+        pc_parent = ProductCategory(description=u'pc_parent')
+        pc_child_1 = ProductCategory(description=u'pc_child_1',
+                                     parent=pc_parent)
+        pc_child_2 = ProductCategory(description=u'pc_child_2',
+                                     parent=pc_parent)
+
+        pc_child_11 = ProductCategory(description=u'pc_child_11',
+                                       parent=pc_child_1)
+        pc_child_12 = ProductCategory(description=u'pc_child_12',
+                                       parent=pc_child_1)
+        pc_child_21 = ProductCategory(description=u'pc_child_21',
+                                       parent=pc_child_2)
+        pc_child_22 = ProductCategory(description=u'pc_child_22',
+                                       parent=pc_child_2)
+
+        pc_child_211 = ProductCategory(description=u'pc_child_211',
+                                          parent=pc_child_21)
+
+        self.db.session.add(pc_parent)
+        self.db.session.commit()
+
+        assert list(pc_child_211.get_children_recursively()) == []
+        assert list(pc_child_21.get_children_recursively()) == [pc_child_211]
+        assert sorted(list(pc_child_1.get_children_recursively())) == \
+                sorted([pc_child_12, pc_child_11])
+        assert sorted(list(pc_child_2.get_children_recursively())) == \
+                sorted([pc_child_211, pc_child_21, pc_child_22])
+        assert sorted(list(pc_parent.get_children_recursively())) == \
+                sorted([pc_child_1, pc_child_2, pc_child_11, pc_child_12,
+                        pc_child_21, pc_child_22, pc_child_211])
 
 
 class TestProduct(TestCase):
+
+    def test_default_status(self):
+        p = Product(sku=u'1', description=u'p1', price=Decimal('1'))
+        self.db.session.add(p)
+        self.db.session.commit()
+        assert p.status == Product.STATUS_AVAILABLE
+
+    def test_default_product_type(self):
+        p = Product(sku=u'1', description=u'p1', price=Decimal('1'))
+        self.db.session.add(p)
+        self.db.session.commit()
+        assert p.product_type == Product.TYPE_PERMANENT
 
     def test_raises_for_commit_without_price(self):
         p = Product(sku=u'1', description=u'p1')
