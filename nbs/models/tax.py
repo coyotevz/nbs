95dbdd7e675c06762e3d94c2dc8373feead9ec63
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-11-21 03:26:55 -0300

    Work on Tax model

--- nbs/models/tax.py
+++ nbs/models/tax.py
@@ -2,8 +2,23 @@
 
 from nbs.models import db
 
-class TaxConstant(db.Model):
-    __tablename__ = 'tax_constant'
+class Tax(db.Model):
+    __tablename__ = 'tax'
+
+    #: used for sale operations
+    OPERATION_SALE = u'OPERATION_SALE'
+
+    #: used for purchase operation
+    OPERATION_PURCHASE = u'OPERATION_PURCHASE'
+
+    #: used for other operations
+    OPERATION_OTHER = u'OPERATION_OTHER'
+
+    _operation_types = {
+        OPERATION_SALE: u'Venta',
+        OPERATION_PURCHASE: u'Compra',
+        OPERATION_OTHER: u'Otras operaciones',
+    }
 
     id = db.Column(db.Integer, primary_key=True)
 
@@ -15,3 +30,12 @@ class TaxConstant(db.Model):
 
     #: applicable tax value
     value = db.Column(db.Numeric(10, 4), nullable=False)
+
+    #: operation type
+    operation_type = db.Column(db.Enum(*_operation_types.keys(),
+                                       name='tax_constant_operation_type'),
+                               default=OPERATION_SALE)
+
+    @property
+    def operation_type_str(self):
+        return self._operation_types[self.operation_type]
