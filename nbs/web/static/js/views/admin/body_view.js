8157a0ad2c8e97ff59a035683bf2bbe438dcc518
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-12-06 16:49:39 -0300

    Frontend: Some new regions for header & body

--- nbs/web/static/js/views/admin/body_view.js
+++ nbs/web/static/js/views/admin/body_view.js
@@ -6,6 +6,16 @@ define([
   var BodyView = View.extend({
     template: 'admin/body.html',
 
+    regions: {
+      sidebar_header: '',
+      sidebar: '',
+      sidebar_footer: '',
+      toolbox: '',
+      content_header: '',
+      content: '',
+      content_footer: '',
+    },
+
     initialize: function() {
       BodyView.__super__.initialize.apply(this, arguments);
       console.log('BodyView#initialize(%s)', this.cid);
--- nbs/web/static/js/views/admin/header_view.js
+++ nbs/web/static/js/views/admin/header_view.js
@@ -6,6 +6,11 @@ define([
   var HeaderView = View.extend({
     template: 'admin/header.html',
 
+    regions: {
+      ribbon: '',
+      banner: '',
+    },
+
     initialize: function() {
       HeaderView.__super__.initialize.apply(this, arguments);
       console.log('HeaderView#initialize(%s)', this.cid);
