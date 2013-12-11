e4a4716b0f2157f7df82701cbd8ffd39cc36fc34
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-12-11 01:51:33 -0300

    Frontend: more work on scss

--- nbs/web/static/js/views/admin/body_view.js
+++ nbs/web/static/js/views/admin/body_view.js
@@ -1,6 +1,7 @@
 define([
+  'jquery',
   'views/base/view',
-], function(View) {
+], function($, View) {
   "use strict";
 
   var BodyView = View.extend({
@@ -19,6 +20,27 @@ define([
     initialize: function() {
       BodyView.__super__.initialize.apply(this, arguments);
       console.log('BodyView#initialize(%s)', this.cid);
+      console.log(this.$('#content_wrapper'));
+    },
+
+    render: function() {
+      BodyView.__super__.render.apply(this, arguments);
+      $(window).resize(this.resize);
+      $(window).focus(this.resize);
+      this.resize();
+    },
+
+    resize: function() {
+      var contentWrapper = this.$('#content_wrapper'),
+          scrollWrapper = this.$('#scroll_wrapper'),
+          contentAvlWidth = $(window).width() - $('aside').width(),
+          contentAvlHeight = $(window).height() - contentWrapper.position().top;
+
+      console.log('window.width:', $(window).width(), 'aside.width:', $('aside').width());
+
+      contentWrapper.width(contentAvlWidth);
+      contentWrapper.height(contentAvlHeight);
+      scrollWrapper.height(contentAvlHeight);
     },
   });
 
