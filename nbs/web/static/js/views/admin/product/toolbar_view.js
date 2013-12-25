8c9460e3121cfa665fca69dead57192d52feb0c8
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-12-25 05:40:11 -0300

    Frontend: Adding functionality to

--- nbs/web/static/js/views/admin/product/toolbar_view.js
+++ nbs/web/static/js/views/admin/product/toolbar_view.js
@@ -6,12 +6,21 @@ define([
   var ProductToolbarView = View.extend({
     template: 'admin/product/toolbar.html',
 
+    initialize: function() {
+      ProductToolbarView.__super__.initialize.apply(this, arguments);
+      this.delegate('click', '.btn[name="back"]', this.goBack);
+    },
+
     getTemplateData: function() {
       return _.extend(
         ProductToolbarView.__super__.getTemplateData.apply(this, arguments),
         {cid: this.cid}
       );
     },
+
+    goBack: function() {
+      window.history.back();
+    }
   });
 
   return ProductToolbarView;
