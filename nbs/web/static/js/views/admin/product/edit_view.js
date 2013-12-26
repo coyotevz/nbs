652e7473fdc61fa3252af341ef302a20b6a331e5
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-12-25 23:37:13 -0300

    Adding EditToolbarView

--- nbs/web/static/js/views/admin/product/edit_view.js
+++ nbs/web/static/js/views/admin/product/edit_view.js
@@ -3,12 +3,26 @@ define([
 ], function(View) {
   "use strict";
 
+  var EditToolbarView = View.extend({
+    template: 'admin/product/edit_toolbar.html',
+
+    initialize: function() {
+      EditToolbarView.__super__.initialize.apply(this, arguments);
+      this.delegate('click', '.btn[name="back"]', this.goBack);
+    },
+
+    goBack: function() {
+      window.history.back();
+    },
+  });
+
   var ProductEditView = View.extend({
     template: 'admin/product/edit.html',
     noWrap: true,
 
     initialize: function(params) {
       ProductEditView.__super__.initialize.apply(this, arguments);
+      this.view = new EditToolbarView();
       console.log('ProductEditView');
     },
 
