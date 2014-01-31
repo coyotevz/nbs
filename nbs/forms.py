98186b5396aa709b3cc7dc5e617b8fadd016536f
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-01-31 15:40:12 -0300

    Update dependencies

--- nbs/forms.py
+++ nbs/forms.py
@@ -46,6 +46,10 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
 
+    @classmethod
+    def get_session():
+        return db.session
+
 
 class ProductSupplierInfoForm(ModelForm):
 
