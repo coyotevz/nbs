7837018ed887f074424ac243ae5715a21af744ae
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-02-07 04:05:45 -0300

    Dialog modal style

--- nbs/web/static/js/views/dialog.js
+++ nbs/web/static/js/views/dialog.js
@@ -7,18 +7,29 @@ define([
     template: 'dialog.html',
     noWrap: true,
 
-    initialize: function() {
-      DialogView.__super__.initialize.apply(this, arguments);
-      console.log('DialogView#initialize');
+    events: {
+      'click .modal-close': 'hide',
+      'click [name=save]': 'save',
+      'click [name=close]': 'hide',
     },
 
     render: function() {
       DialogView.__super__.render.apply(this, arguments);
+      this.delegate('shown.bs.modal', this.reposition);
       this.$el.modal({
         show: false,
       });
+      this.$d = this.$('.modal-dialog');
+
       // Grab global copy of this instance
       window._dialog = this;
+    },
+
+    reposition: function() {
+      this.$d.css({
+        'left': ($(window).width() - this.$d.width()) / 2,
+        'top': ($(window).height() - this.$d.height()) / 2,
+      });
     },
 
     show: function() {
@@ -32,6 +43,11 @@ define([
     toggle: function() {
       this.$el.modal('toggle');
     },
+
+    save: function() {
+      console.log("action save on modal dialog");
+      this.hide();
+    }
   });
 
   return DialogView;
