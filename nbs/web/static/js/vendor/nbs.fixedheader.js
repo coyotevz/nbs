76b4b0995bf9f0fe7fbf83a8239e3a80c8594a2c
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-02-17 16:25:36 -0300

    Progress on implementation of fixedHeader plugin

--- nbs/web/static/js/vendor/nbs.fixedheader.js
+++ nbs/web/static/js/vendor/nbs.fixedheader.js
@@ -9,8 +9,24 @@ define([
 
   $.fn.fixedHeader = function(options) {
     return this.each(function() {
-      var $e = $(this);
-      console.log('fixedHeader plugin:', $e);
+      var offset, scrollTop, $cloned,
+          $e = $(this),
+          $h = $('thead tr', $e);
+      offset = $e.offset();
+      scrollTop = $(window).scrollTop();
+
+      if ((scrollTop > offset.top) && scrollTop < offset.top + $e.height()) {
+        $h.css({
+          'visibility': 'visible',
+          'top': Math.min(scrollTop - offset.top, $e.height() - $h.height()) + "px",
+        });
+      } else {
+        $h.css({
+          'visibility': 'hidden',
+          'top': '0px',
+        });
+      }
+
     });
   }
 });
