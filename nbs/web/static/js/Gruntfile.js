2a284f05bd9e8dfe897334876116d42fc8340543
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-02-21 16:12:40 -0300

    Add jshint task

--- nbs/web/static/js/Gruntfile.js
+++ nbs/web/static/js/Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    /* define tasks */
+    jshint: {
+      files: ['Gruntfile.js', 'app/**/*.js', '!app/vendor/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      }
+    },
+    requirejs: {},
+    nunjucks: {},
   });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-nunjucks');
+
+  grunt.registerTask('default', ['jshint']);
 };
