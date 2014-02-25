c9660219c69b1a5a39fe744f1ad07997c23335f0
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-02-25 01:45:26 -0300

    working on pos controller

--- nbs/web/static/js/app/controllers/pos_controller.js
+++ nbs/web/static/js/app/controllers/pos_controller.js
@@ -1,17 +1,32 @@
 define([
   'chaplin',
+  'views/standard_view',
+  'views/dialog',
+  'models/document',
   'views/pos/document_view',
-], function(Chaplin, DocumentView) {
+], function(Chaplin,
+            StandardView,
+            DialogView,
+            Document,
+            DocumentView) {
   "use strict";
 
   var PosController = Chaplin.Controller.extend({
-
     title: 'Point of Sale',
 
-    index: function(params) {
-      this.view = new DocumentView();
-    }
+    beforeAction: function(params, route) {
+      this.reuse('main', StandardView, {region: 'main'});
+      this.reuse('dialog', DialogView, {region: 'main'});
+    },
 
+    index: function(params) {
+      var model = new Document();
+      model.fetch();
+      this.view = new DocumentView({
+        model: model,
+        region: 'body',
+      });
+    }
   });
 
   return PosController;
