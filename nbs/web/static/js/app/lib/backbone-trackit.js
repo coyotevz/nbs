1e43a800383dada86128e63038e33134f446bb25
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-06-06 03:03:23 -0300

    More work on trackit custom code

--- nbs/web/static/js/app/lib/backbone-trackit.js
+++ nbs/web/static/js/app/lib/backbone-trackit.js
@@ -18,7 +18,6 @@ define([
 
   _.extend(Backbone.Model.prototype, {
 
-  unsaved: {},
     _trackingChanges: false,
     _originalAttrs: {},
     _unsavedChanges: {},
@@ -27,6 +26,7 @@ define([
     // between saves.
     startTracking: function() {
       this._trackingChanges = true;
+      this._resetTracking();
       return this;
     },
 
@@ -34,6 +34,8 @@ define([
     // and stops tracking attribute changes
     stopTracking: function() {
       this._trackingChanges = false;
+      this._originalAttrs = {};
+      this._unsavedChanges = {};
       return this;
     },
 
@@ -42,16 +44,21 @@ define([
     },
 
     isChanged: function() {
+      return !_.isEmpty(this._unsavedChanges);
     },
 
     // Gets rid of accrued changes and resets state.
     restartTracking: function() {
+      this._resetTracking();
       return this;
     },
 
     // Restores this model's attributes to their original values since tracking
     // started, the last save, or last restart.
     resetAttributes: function() {
+      if (!this._trackingChanges) return;
+      this.attributes = this._originalAttrs;
+      this._resetTracking();
       return this;
     },
 
@@ -61,13 +68,66 @@ define([
     // Like `changedAttributes`, an external attributes hash can be passed in,
     // returning the attributes in that hash which differ from the model.
     unsavedAttributes: function(attrs) {
+      if (!attrs) return _.isEmpty(this._unsavedChanges) ? false : _.clone(this._unsavedChanges);
+      var val, changed = false, old = this._unsavedChanges;
+      for (var attr in attrs) {
+        if (_.isEqual(old[attr], (val = attrs[attr]))) continue;
+        (changed || (changed = {}))[attr] = val;
+      }
+      return changed;
+    },
 
+    _resetTracking: function() {
+      this._originalAttrs = _.clone(this.attributes);
+      this._unsavedChanges = {};
     },
   });
 
   // Wrap `model.set()` and update the internal unsaved changes record keeping.
   Backbone.Model.prototype.set = _.wrap(Backbone.Model.prototype.set, function(oldSet, key, val, options) {
-  })
+    var attrs, ret;
+
+    if (key == null) return this;
+    // Handle both `"key", "value"` and `{key: value}` -style arguments.
+    if (typeof key === 'object') {
+      attrs = key;
+      options = val;
+    } else {
+      (attrs = {})[key] = val;
+    }
+    options || (options = {});
+
+    // Delegate to Backbone's set.
+    ret = oldSet.call(this, attrs, options);
+
+    if (this._trackingChanges && !options.silent) {
+      _.each(attrs, _.bind(function(val, key) {
+        if (_.isEqual(this._originalAttrs[key], val))
+          delete this._unsavedChanges[key];
+        else
+          this._unsavedChanges[key] = val;
+      }, this));
+    }
+    return ret;
+  });
+
+  // Intercept `model.save()` and reset tracking/unsaved changes if it was
+  // successfull.
+  Backbone.sync = _.wrap(Backbone.sync, function(oldSync, method, model, options) {
+    options || (options = {});
+
+    if (method == 'update') {
+      options.success = _.wrap(options.success, _.bind(function(oldSuccess, data, textStatus, jqXHR) {
+        var ret;
+        if (oldSuccess) ret = oldSuccess.call(this, data, textStatus, jqXHR);
+        if (model._trackingChanges) {
+          model._resetTracking();
+        }
+        return ret;
+      }, this));
+    }
+    return oldSync(method, model, options);
+  });
 
   // Don't return anything
 });
